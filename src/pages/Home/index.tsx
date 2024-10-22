254565e9ad26380e7448708a39e1682e313f1306
Juan Esteban García Martínez <juan.garcia@premexcorp.com>
2024-10-22 16:19:04 -0500

    Implement data fetching

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,35 @@
+import { useState, useEffect } from "react";
+
 import Layout from "../Layout";
 import Card from "../../components/Card";
+import { Product } from "../../types";
+
+const apiUrl = "https://fakestoreapi.com";
 
 export default function Home() {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch(`${apiUrl}/products`);
+        const data = await res.json();
+        setProducts(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <Layout>
       Home
-      <Card />
+      <div className="grid gap-4 grid-cols-4 w-full max-w-screen-lg">
+        {products.map((product: Product) => (
+          <Card key={product.id} product={product} />
+        ))}
+      </div>
     </Layout>
   );
 }
